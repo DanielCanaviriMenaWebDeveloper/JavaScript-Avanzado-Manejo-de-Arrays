dfe448782385532a366e63238bcc6a44255b26d1
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2022-06-11 23:19:53 -0400

    push

---
+++ 1. Métodos push, pop, shift y unshift/app.js
@@ -0,0 +1,23 @@
+// Conocidos como mutators porque alteran la estructura del Array
+// push, pop, shift, unshift
+// push : Permite agregar elementos al array
+
+var arrayNumeros = [];
+
+arrayNumeros.push(10);
+console.log(arrayNumeros);
+
+arrayNumeros.push(20);
+console.log(arrayNumeros);
+
+arrayNumeros.push(30);
+console.log(arrayNumeros);
+
+arrayNumeros.push(40);
+console.log(arrayNumeros);
+
+arrayNumeros.push(50);
+console.log(arrayNumeros);
+
+arrayNumeros.push(60, 70, 80);
+console.log(arrayNumeros);
